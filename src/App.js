8651e6ef2038ec9e12b5f2df40ac9be109d80d26
mounick23 <67328316+mounick23@users.noreply.github.com>
2020-08-27 23:39:49 +0530

    Initial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>my name is hawk i create new things</h2>
+    </div>
+  );
+}
+
+export default App;
